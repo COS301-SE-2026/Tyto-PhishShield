df036076746fef5ae97fd9f8da5fb4a74c8dac35
Josua-Louw <u24569772@tuks.co.za>
2026-05-11 10:11:08 +0200

    set up swagger docs and ports

--- Project/Backend/Accounts_Service/src/main.ts
+++ Project/Backend/Accounts_Service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Accounts Service')
+    .setDescription('The Accounts Service description')
+    .setVersion('1.0')
+    .addTag('accounts-service')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('accounts-api', app, documentFactory);
+  
+  await app.listen(process.env.Accounts_Service_PORT ?? 3002);
 }
 bootstrap();
