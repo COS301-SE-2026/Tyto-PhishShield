1119c19d5e88f6644a617dcbb862cdf004a1c099
Heindrich-Jansen <heindrichjansen@gmail.com>
2026-05-12 23:09:56 +0200

    Refactor app module and main entry point for improved structure and validation setup

--- Project/Backend/Accounts_Service/src/main.ts
+++ Project/Backend/Accounts_Service/src/main.ts
@@ -1,19 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Accounts Service')
-    .setDescription('The Accounts Service description')
-    .setVersion('1.0')
-    .addTag('accounts-service')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('accounts-api', app, documentFactory);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(process.env.Accounts_Service_PORT ?? 3002);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`accounts Service is running on port ${port}`);
 }
 bootstrap();
