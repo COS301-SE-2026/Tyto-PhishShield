71094c45eea51682d39a2339e92cd5692ff0151a
Heindrich Jansen <heindrichjansen@gmail.com>
2026-05-12 17:35:37 +0200

    PostgreSQL code for a users table.

---
+++ Project/Backend/Accounts_Service/src/users/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+    Entity, Column, PrimaryGeneratedColumn, 
+    CreateDateColumn, UpdateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  ANALYST = 'analyst',
+  USER = 'user',
+}
+
+@Entity('users')
+export class User {
+    PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    //this is how we link DB row to Auth0 account
+    @Column({ unique: true })
+    auth0Id: string;
+    
+    @Column({ unique: true })
+    email: string;
+
+    @Column({ nullable: true })
+    name: string;
+
+    @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+    role: UserRole;
+
+    @CreateDateColumn()
+    createdAt: Date;
+    
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
