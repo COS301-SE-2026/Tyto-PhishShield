cf878c14a57751ac89f85d02cfbef30859723aa2
DariusErasmus <u24586189@tuks.co.za>
2026-05-13 19:06:09 +0200

    Added db table when building docker-compose.

---
+++ Project/Backend/mailing-service/init-db/01-extensions.sql
@@ -0,0 +1,8 @@
+/*
+ * PROJECT: Tyto-PhishShield
+ * SERVICE: Mailing Service
+ * FILE: 01-extensions.sql
+ * DESCRIPTION: Enables necessary PostgreSQL extensions.
+ */
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
