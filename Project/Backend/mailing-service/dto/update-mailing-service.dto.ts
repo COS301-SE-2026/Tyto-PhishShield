d9461d2b83f0fae01deceb51ea723d70580bdc87
DariusErasmus <u24586189@tuks.co.za>
2026-05-13 19:05:29 +0200

    Managed the entities and dto files needed for generating emails.

--- Project/Backend/mailing-service/dto/update-mailing-service.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMailingServiceDto } from './create-mailing-service.dto';
-
-export class UpdateMailingServiceDto extends PartialType(CreateMailingServiceDto) {}
