936fa1b5893acd6547ed8a8ead29316546675cad
Josua-Louw <u24569772@tuks.co.za>
2026-05-18 12:33:39 +0200

    fixed linting files

--- Project/Backend/mailing-service/eslint.config.mjs
+++ Project/Backend/mailing-service/eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+//import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 export default defineConfig([
     {
@@ -9,6 +9,6 @@ export default defineConfig([
     },
     { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
     tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    //pluginReact.configs.flat.recommended,
 ]);
 //# sourceMappingURL=eslint.config.mjs.map
--- Project/Backend/mailing-service/eslint.config.mts
+++ Project/Backend/mailing-service/eslint.config.mts
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -10,5 +10,5 @@ export default defineConfig([
   },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+ // pluginReact.configs.flat.recommended,
 ]);
