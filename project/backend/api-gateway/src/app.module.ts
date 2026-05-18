ea2bd74592a310725ecfc0b2256f9c31e9f2c96a
Heindrich-Jansen <heindrichjansen@gmail.com>
2026-05-19 00:12:58 +0200

    refactor(api-gateway): update dependencies and remove proxy middleware

--- project/backend/api-gateway/src/app.module.ts
+++ project/backend/api-gateway/src/app.module.ts
@@ -1,20 +1,12 @@
-import {
-  Module,
-  MiddlewareConsumer,
-  NestModule,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AccountsModule } from './accounts/accounts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { ProxyMiddleware } from './proxy/proxy.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(ProxyMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
