e67603e338360baef37e2153a0f36fca8c2118cb
DariusErasmus <u24586189@tuks.co.za>
2026-05-19 09:55:58 +0200

    Fixed dependency error in module file and updated exclude list in tsconfig.json to exclude jest.config.ts.

--- project/backend/mailing-service/src/email/email.module.ts
+++ project/backend/mailing-service/src/email/email.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GeneratedEmail } from '../entities/generated-emails.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GeneratedEmail])],
   controllers: [EmailController],
   providers: [EmailService],
 })
