d7e55232a34cc36fadb13ef57fbb632335ef625e
DariusErasmus <u24586189@tuks.co.za>
2026-05-20 17:18:35 +0200

    Added xp and report handle

--- project/backend/api-gateway/src/app.module.ts
+++ project/backend/api-gateway/src/app.module.ts
@@ -5,14 +5,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailingController } from './mailing/mailing.controller';
 import { MailingModule } from './mailing/mailing.module';
+import { ReportController } from './report/report.controller';
+import { ReportModule } from './report/report.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AccountsModule,
     MailingModule,
+    ReportModule,
   ],
-  controllers: [AppController, MailingController],
+  controllers: [AppController, MailingController, ReportController],
   providers: [AppService],
 })
 export class AppModule {}
