60180d9e023487f8f2557e44026687e14170271c
Josua-Louw <u24569772@tuks.co.za>
2026-06-03 11:59:08 +0200

    added microservice even transport ability

--- project/backend/xp-service/src/app.module.ts
+++ project/backend/xp-service/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'EVENT_BUS',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
+          queue: 'xp-service.queue',
+          queueOptions: { 
+            durable: true 
+          }
+        }
+      }
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- project/backend/xp-service/src/main.ts
+++ project/backend/xp-service/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.TCP,
+      options: {
+        port: Number(process.env.PORT ?? 3000),
+      },
+    },
+  );
+
+  if (process.env.RABBITMQ_URL) {
+    app.connectMicroservice<MicroserviceOptions>(
+      {
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: 'xp-service.queue',
+          queueOptions: {
+            durable: true
+          }
+        }
+      }
+    );
+  }
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
+  console.log("XP service listening on port: " + process.env.PORT);
 }
 bootstrap();
