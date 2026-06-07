13b685f2078d2d47c61a9102cb0eca7a64722695
Josua-Louw <u24569772@tuks.co.za>
2026-06-07 20:07:21 +0200

    Added tcp and RMQ microservices to accounts and mailing

---
+++ project/backend/mailing-service/src/mailing-service.controller.ts
@@ -0,0 +1,16 @@
+/*
+ * Used to check health of tcp connection from the api-gateway
+*/
+
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+
+@Controller()
+export class MailingServiceController {
+  constructor() {};
+
+  @MessagePattern('health.check')
+  health(): string {
+    return 'ok';
+  }
+}
