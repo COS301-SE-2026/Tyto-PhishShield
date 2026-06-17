1a3069b75c0e98a09b9d7d84b6c4dfdc5ce77444
Heindrich-Jansen <heindrichjansen@gmail.com>
2026-06-17 20:23:34 +0200

    feat: add report entity and create report DTO with validation rules

---
+++ project/backend/report-service/src/report/dto/create-report.dto.ts
@@ -0,0 +1,30 @@
+import { IsString, IsEmail, IsOptional, IsDateString} from 'class-validator';
+
+export class CreateReportDto {
+    @IsString()
+    @IsOptional()
+    outlookMessageId?: string;
+
+    @IsString()
+    @IsOptional()
+    emailSubject?: string;
+
+    @IsString()
+    @IsOptional()
+    emailSender?: string;
+
+    @IsString()
+    @IsOptional()
+    emailBody?: string;
+
+    @IsDateString()
+    @IsOptional()
+    emailReceivedAt?: Date;
+
+    @IsString()
+    @IsOptional()
+    notes?: string;
+
+
+    
+}
