c69f2340729439af23e3211657dd6133c0f0d602
DariusErasmus <u24586189@tuks.co.za>
2026-06-27 10:33:20 +0200

    Added account event managing

--- project/backend/xp-service/src/accounts/accounts.service.ts
+++ project/backend/xp-service/src/accounts/accounts.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+// TODO add comments / add logs / find out what is happening
+
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { User } from '../dto/user.dto';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  private readonly logger = new Logger(AccountsService.name);
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(user: User): Promise<void> {
+    await this.userRepository.upsert(
+      {
+        id: user.id,
+        auth0Id: user.auth0Id,
+        name: user.name,
+        email: user.email,
+        department: user.department,
+      },
+      { conflictPaths: ['auth0Id'], skipUpdateIfNoValuesChanged: true },
+    );
+    this.logger.log(`Upserted user ${user.auth0Id}`);
+  }
+}
