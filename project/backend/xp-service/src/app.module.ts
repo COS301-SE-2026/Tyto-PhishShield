223488baae556b3da43084124caebd2d7895b2d6
DariusErasmus <u24586189@tuks.co.za>
2026-06-27 10:33:37 +0200

    Connected the db

--- project/backend/xp-service/src/app.module.ts
+++ project/backend/xp-service/src/app.module.ts
@@ -2,9 +2,37 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
+import { XpModule } from './xp/xp.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { XpEntity } from './entities/xp.entity';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('XP_DB_CONTAINER'),
+        port: configService.get<number>('XP_DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('XP_DB_NAME'),
+        synchronize: configService.get<string>('DB_SYNC', 'true') === 'true',
+        entities: [UserEntity, XpEntity],
+        autoLoadEntities: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([UserEntity, XpEntity], 'xp-db'),
+    AccountsModule,
+    XpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
