4ca0d12b88ab95441d745c9794f168e2d5572c78
Josua-Louw <u24569772@tuks.co.za>
2026-07-13 09:48:48 +0200

    added switch for logging on github actions

--- project/backend/api-gateway/src/logger/logger.service.ts
+++ project/backend/api-gateway/src/logger/logger.service.ts
@@ -3,6 +3,21 @@ import 'winston-daily-rotate-file';
 const { combine, timestamp, json, errors } = winston.format;
 const logDir = process.env.LOG_DIR ?? '/var/log/api-gateway';
 
+const isTestOrCi = process.env.NODE_ENV === 'test' || process.env.CI === 'true' || process.env.LOG_TO_FILE === 'false';
+
+const normalTransports: winston.transport[] = [ new winston.transports.Console() ];
+
+if (!isTestOrCi) {
+  normalTransports.push(
+    new winston.transports.DailyRotateFile({
+      dirname: logDir,
+      filename: 'standard-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      maxFiles: '14d',
+    })
+  );
+}
+
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || undefined,
   format: combine(timestamp(), errors({ stack: true }), json()),
@@ -15,7 +30,9 @@ export const logger = winston.createLogger({
       maxFiles: '14d',
     }),
   ],
-  exceptionHandlers: [
+  exceptionHandlers: isTestOrCi ?
+    [new winston.transports.Console() ] :
+    [
     new winston.transports.DailyRotateFile({
       dirname: logDir,
       filename: 'exceptions-%DATE%.log',
@@ -23,7 +40,9 @@ export const logger = winston.createLogger({
       maxFiles: '14d',
     }),
   ],
-  rejectionHandlers: [
+  rejectionHandlers: isTestOrCi ?
+    [new winston.transports.Console() ] :
+    [
     new winston.transports.DailyRotateFile({
       dirname: logDir,
       filename: 'rejections-%DATE%.log',
