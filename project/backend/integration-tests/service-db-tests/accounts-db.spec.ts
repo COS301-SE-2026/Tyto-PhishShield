45e13d06fb0cae8f679ea28ada1c7807e89c4397
Josua-Louw <u24569772@tuks.co.za>
2026-07-18 16:59:17 +0200

    updated accounts testing to only test user module which interacts with the db

--- project/backend/integration-tests/service-db-tests/accounts-db.spec.ts
+++ project/backend/integration-tests/service-db-tests/accounts-db.spec.ts
@@ -4,6 +4,10 @@ import { NestApplication, NestFactory } from '@nestjs/core';
 import { DataSource } from 'typeorm';
 import { applyMixins } from 'rxjs/internal/util/applyMixins';
 import { StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from '../../accounts-service/src/users/entities/user.entity';
+import { Otp } from '../../accounts-service/src/otp/otp.entity';
 
 describe('Accounts to DB integration Test', () => {
     let accountsApp: NestApplication;
@@ -15,13 +19,32 @@ describe('Accounts to DB integration Test', () => {
 
         process.env.DB_HOST = accountsDB.getHost();
         process.env.DB_PORT = accountsDB.getPort().toString();
-        process.env.DB_USER = accountsDB.getUsername();
+        process.env.DB_USERNAME = accountsDB.getUsername();
         process.env.DB_PASSWORD = accountsDB.getPassword();
         process.env.DB_NAME = accountsDB.getDatabase();
 
-        const { AppModule } = await import('../../accounts-service/src/app.module');
+        const { UsersModule } = await import('../../accounts-service/src/users/users.module');
+        const accountsTestModule = await Test.createTestingModule({
+            imports: [
+                TypeOrmModule.forRootAsync({
+                imports: [ConfigModule],
+                inject: [ConfigService],
+                useFactory: (config: ConfigService) => ({
+                    type: 'postgres',
+                    host: config.get('DB_HOST', 'localhost'),
+                    port: config.get<number>('DB_PORT', 5432),
+                    username: config.get('DB_USERNAME'),
+                    password: config.get('DB_PASSWORD'),
+                    database: config.get('DB_NAME'),
+                    entities: [User, Otp],
+                    synchronize: true,
+                    }),
+                }),
+                UsersModule
+            ],
 
-        accountsApp = await NestFactory.create(AppModule, {logger: false});
+        }).compile();
+        accountsApp = accountsTestModule.createNestApplication();
         await accountsApp.init();
 
         dataSource = accountsApp.get(DataSource);
