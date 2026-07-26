c5cd6e7687fc0424bbf8d00018ad6e4242c0862e
Heindrich-Jansen <heindrichjansen@gmail.com>
2026-07-26 17:55:44 +0200

    Fix typo in Auth0UserResponse and update email verification check in AuthService

--- project/backend/accounts-service/src/auth/auth.service.ts
+++ project/backend/accounts-service/src/auth/auth.service.ts
@@ -33,7 +33,7 @@ interface Auth0UserResponse {
   user_id: string;
   email: string;
   name: string;
-  email_verfied: boolean;
+  email_verified: boolean;
 }
 
 interface Auth0LoginResponse {
@@ -153,7 +153,7 @@ export class AuthService {
           },
         ),
       );
-      if (data && !data.email_verfied) {
+      if (data && !data.email_verified) {
         throw new UnauthorizedException(
           'Email not verified. Please verify your email before logging in. (Note it may take time for the email to be marked as verified.)',
         );
