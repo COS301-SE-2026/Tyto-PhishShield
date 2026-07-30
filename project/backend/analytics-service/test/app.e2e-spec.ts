4af1b674b01e59b09d151d7aa5d1ae76e50ca0a9
Josua-Louw <u24569772@tuks.co.za>
2026-07-30 08:09:50 +0200

    added report and analytics to integration pipeline

--- project/backend/analytics-service/test/app.e2e-spec.ts
+++ project/backend/analytics-service/test/app.e2e-spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+
+const mockAmqpConnection = { publish: jest.fn() };
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      controllers: [AppController],
+      providers: [AppService, { provide: AmqpConnection, useValue: mockAmqpConnection },],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
     await app.init();
   });
 
