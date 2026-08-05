a4d8d34d53ec6c1674c9cd88bfca62406db49d0c
DariusErasmus <u24586189@tuks.co.za>
2026-08-05 16:13:29 +0200

    Adjusted dto's to use auth0Id and new name of Emails db

--- project/backend/mailing-service/src/dto/send-batch-email.dto.ts
+++ project/backend/mailing-service/src/dto/send-batch-email.dto.ts
@@ -1,9 +1,9 @@
-import { IsArray, IsEmail, ArrayMinSize, ArrayMaxSize } from 'class-validator';
+import { IsArray, ArrayMinSize, ArrayMaxSize, IsString } from 'class-validator';
 
 export class SendBatchEmailDto {
   @IsArray()
   @ArrayMinSize(1)
   @ArrayMaxSize(100)
-  @IsEmail({}, { each: true })
-  recipients: string[];
+  @IsString()
+  auth0Id: string[];
 }
