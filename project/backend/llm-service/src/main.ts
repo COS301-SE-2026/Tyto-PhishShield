3036173d1b33c79ed17ae581f224c8e9d30d6c1b
Josua-Louw <u24569772@tuks.co.za>
2026-08-06 09:31:57 +0200

    updated llm main

--- project/backend/llm-service/src/main.ts
+++ project/backend/llm-service/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: Number(process.env.TCP_PORT ?? 3000),
+      },
+    },
+  );
+
+  if (process.env.RABBITMQ_URL) {
+    app.connectMicroservice<MicroserviceOptions>(
+      {
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL],
+          queue: 'llm.queue',
+          queueOptions: {
+            durable: true
+          }
+        }
+      }
+    );
+  }
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
+  console.log("llm-service listening on port: " + process.env.PORT);
+  console.log("llm tcp service listening on port: " + process.env.TCP_PORT);
 }
 bootstrap();
