d65ca67a8b4f850f14c9bbf08375ce765565d7ba
DariusErasmus <u24586189@tuks.co.za>
2026-08-07 16:55:21 +0200

    Removed email-status in the mailing service.

--- project/backend/mailing-service/src/mailing-service.module.ts
+++ project/backend/mailing-service/src/mailing-service.module.ts
@@ -3,12 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EmailTemplateEntity } from './entities/email-template.entity';
 import { UserEntity } from './entities/user.entity';
-import { EmailStatusEntity } from './entities/email-status.entity';
 import { EmailModule } from './email/email.module';
 import { MailingServiceController } from './mailing-service.controller';
 import { BatchEmailModule } from './batch-email/batch-email.module';
 import { mailingRabbitMQModule } from './rabbitmq.module';
-import { StatusModule } from './status/status.module';
 
 @Module({
   imports: [
@@ -27,17 +25,15 @@ import { StatusModule } from './status/status.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('MAILING_DB_NAME'),
         synchronize: configService.get<string>('DB_SYNC', 'true') === 'true',
-        entities: [EmailTemplateEntity, UserEntity, EmailStatusEntity],
+        entities: [EmailTemplateEntity, UserEntity],
         autoLoadEntities: true,
       }),
     }),
     TypeOrmModule.forFeature([EmailTemplateEntity]),
     TypeOrmModule.forFeature([UserEntity]),
-    TypeOrmModule.forFeature([EmailStatusEntity]),
     mailingRabbitMQModule,
     EmailModule,
     BatchEmailModule,
-    StatusModule,
   ],
   controllers: [MailingServiceController],
 })
