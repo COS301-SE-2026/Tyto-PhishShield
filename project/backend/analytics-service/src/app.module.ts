7658d7de4817148dabbe73630331b736d13bce49
DariusErasmus <u24586189@tuks.co.za>
2026-08-07 16:52:48 +0200

    Fixed errors in analytics service and added dto.

--- project/backend/analytics-service/src/app.module.ts
+++ project/backend/analytics-service/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
+import { EmailStatusModule } from './email-status/email-status.module';
 
+// Please also add the EmailStatusEntity to the TypeOrmModule.forFeature()
 @Module({
-  imports: [AccountsModule],
+  imports: [AccountsModule, EmailStatusModule],
   controllers: [AppController],
   providers: [AppService],
 })
