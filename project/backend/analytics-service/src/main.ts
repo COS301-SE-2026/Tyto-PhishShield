7cbef44d191d68c3913f16ae15266628660a6a20
Heindrich-Jansen <heindrichjansen@gmail.com>
2026-08-12 22:32:16 +0200

    Import ValidationPipe in main.ts for global validation setup

--- project/backend/analytics-service/src/main.ts
+++ project/backend/analytics-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
