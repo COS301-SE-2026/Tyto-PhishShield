56dbb66a53a7118160a1cf3304ec014328c72e3e
DariusErasmus <u24586189@tuks.co.za>
2026-08-20 18:53:56 +0200

    Added email-details.entity.ts

---
+++ project/backend/xp-service/src/entities/email-details.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('email_details')
+export class EmailDetailsEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Index({ unique: true })
+  @Column()
+  token: string;
+
+  @Column()
+  auth0Id: string;
+
+  @Column()
+  referenceNumber: string;
+
+  @Column()
+  emailId: string;
+
+  @Column({ type: 'timestamptz' })
+  scheduledAt: Date;
+
+  @Column({ default: false })
+  clicked: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
