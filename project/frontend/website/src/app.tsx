46807d07cf66bccae1c03c17c8a7436d7966b589
Nico-Theron <u23565722@tuks.co.za>
2026-08-20 11:03:18 +0200

    Updated routes in app.tsx

--- project/frontend/website/src/app.tsx
+++ project/frontend/website/src/app.tsx
@@ -10,8 +10,9 @@ import { Reports } from './pages/analytics/reports';
 import { Training } from './pages/training/training';
 import { Waves } from './pages/waves/waves';
 import { Emails } from './pages/emails/emails';
+import { ManageEmailTemplates } from "./pages/emails/manage-email-templates";
 import { ScheduleWave } from './pages/waves/schedule-wave';
-import { CreateEmail } from './pages/waves/create-email';
+import { CreateEmail } from './pages/emails/create-email';
 import { SendEmail } from './pages/waves/send-existing-email';
 import { Users } from './pages/users/users';
 import { UserProfile } from './pages/users/user-profile';
@@ -40,9 +41,10 @@ function App() {
       <Route path="/analytics" element={ <ProtectedRoute minRole="analyst"><Analytics onNavigate={handleNavigate} activePath="/analytics" /></ProtectedRoute> } />
       <Route path="/analytics/reports" element={ <ProtectedRoute minRole="analyst"><Reports onNavigate={handleNavigate} activePath="/analytics/reports" /></ProtectedRoute> } />
       <Route path="/waves" element={ <ProtectedRoute minRole="analyst"><Waves onNavigate={handleNavigate} activePath="/waves" /></ProtectedRoute> } />
+      <Route path="/emails/templates" element={ <ProtectedRoute minRole="analyst"><ManageEmailTemplates onNavigate={handleNavigate} activePath="/emails" /></ProtectedRoute> } />
       <Route path="/emails" element={ <ProtectedRoute minRole="analyst"><Emails onNavigate={handleNavigate} activePath="/emails" /></ProtectedRoute> } />
       <Route path="/waves/schedule" element={ <ProtectedRoute minRole="admin"><ScheduleWave onNavigate={handleNavigate} activePath="/waves" /></ProtectedRoute> } />
-      <Route path="/waves/create-email" element={ <ProtectedRoute minRole="admin"><CreateEmail onNavigate={handleNavigate} activePath="/waves" /></ProtectedRoute> } />
+      <Route path="/emails/create-email" element={ <ProtectedRoute minRole="admin"><CreateEmail onNavigate={handleNavigate} activePath="/emails" /></ProtectedRoute> } />
       <Route path="/waves/send-email" element={ <ProtectedRoute minRole="admin"><SendEmail onNavigate={handleNavigate} activePath="/waves" /></ProtectedRoute> } />
       <Route path="/users" element={ <ProtectedRoute minRole="analyst"><Users onNavigate={handleNavigate} activePath="/users" /></ProtectedRoute> } />
       <Route path="/users/:id" element={ <ProtectedRoute minRole="analyst"><UserProfileById onNavigate={handleNavigate} /></ProtectedRoute> } />
