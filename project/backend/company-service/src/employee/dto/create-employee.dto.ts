a3681af41ab1ad87fe73314de2ca98aa02565f64
Josua-Louw <u24569772@tuks.co.za>
2026-08-29 11:41:12 +0200

    implmented employee code

--- project/backend/company-service/src/employee/dto/create-employee.dto.ts
+++ project/backend/company-service/src/employee/dto/create-employee.dto.ts
@@ -1,1 +1,21 @@
-export class CreateEmployeeDto {}
+export class CreateEmployeeDto {
+    employeeId!: string;
+
+    email!: string;
+
+    firstName?: string;
+
+    lastName?: string;
+
+    department?: string;
+
+    jobTitle?: string;
+
+    managerId?: string;
+
+    employeeStatus?: string;
+
+    externalId?: string;
+
+    dateImported!: Date;
+}
--- project/backend/company-service/src/employee/employee.service.ts
+++ project/backend/company-service/src/employee/employee.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Employee } from './entities/employee.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EmployeeService {
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
+
+  constructor(
+    @InjectRepository(Employee)
+    private readonly db: Repository<Employee>
+  )
+  {}
+
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    const employee = this.db.create(createEmployeeDto);
+    await this.db.save(employee);
+    return employee;
   }
 
   findAll() {
-    return `This action returns all employee`;
+    return this.db.find();
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} employee`;
+  async findOne(id: string) {
+    return await this.db.findOne({
+      where: {
+        employeeId: id,
+      }
+    });
   }
 
   update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
     return `This action updates a #${id} employee`;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} employee`;
+  async remove(id: string) {
+    const employee = await this.findOne(id);
+    if (!employee) {
+      return false;
+    }
+    const removedEmployee = await this.db.remove(employee);
+    if (!removedEmployee) {
+      return false;
+    }
+    return true;
   }
 }
