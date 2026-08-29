eafe25688f50f09155fa47cd5db735a021d140be
Josua-Louw <u24569772@tuks.co.za>
2026-08-29 18:34:18 +0200

    added shared dto package

---
+++ project/backend/packages/dto/src/company/mapping.dto.ts
@@ -0,0 +1,42 @@
+import { IsOptional, IsString } from "class-validator";
+
+export class mappingDto {
+    @IsString()
+    employeeId!: string;    //'Employee-Number Field',
+
+    @IsString()
+    @IsOptional()
+    email?: string;         //'Work-Email Field',
+
+    @IsString()
+    @IsOptional()
+    firstName?: string;     //'First-Name Field',
+
+    @IsString()
+    @IsOptional()
+    lastName?: string;      //'Surname Field',
+
+    @IsString()
+    @IsOptional()
+    department?: string;    //'Department Field',
+
+    @IsString()
+    @IsOptional()
+    jobTitle?: string;      //'Job-Title Field',
+
+    @IsString()
+    @IsOptional()
+    managerEmail?: string;  //'Manager-Email Field',
+
+    @IsString()
+    @IsOptional()
+    managerId?: string;     //'Manager-ID Field',
+
+    @IsString()
+    @IsOptional()
+    employeeStatus?: string;//'Employment-Status Field',
+
+    @IsString()
+    @IsOptional()
+    externalId?: string;    //'HR-Employee-ID Field',
+}
