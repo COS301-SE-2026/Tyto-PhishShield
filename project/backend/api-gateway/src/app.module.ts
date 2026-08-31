7999a69e6eeeb70185a0908fde94ef62be385109
FrikkieMalan <206756660+FrikkieMalan@users.noreply.github.com>
2026-08-31 16:42:20 +0200

    Update app.module.ts
    Removed WavesModule

--- project/backend/api-gateway/src/app.module.ts
+++ project/backend/api-gateway/src/app.module.ts
@@ -22,7 +22,7 @@ import { WebsocketModule } from './websocket/websocket.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 //import { OtpModule } from './otp/otp.module';
 import { WebhookModule } from './webhook/webhook.module';
-import { WavesModule } from './waves/waves.module';
+//import { WavesModule } from './waves/waves.module';
 import { LlmModule } from './llm/llm.module';
 
 @Module({
@@ -34,7 +34,7 @@ import { LlmModule } from './llm/llm.module';
     XpModule,
     EducationModule,
     AnalyticsModule,
-    WavesModule,
+    //WavesModule,
     // Register each microservice tcp client to the api-gateway
     ClientsModule.register([
       {
@@ -96,7 +96,7 @@ import { LlmModule } from './llm/llm.module';
     ]),
     WebsocketModule,
     WebhookModule,
-    WavesModule,
+    //WavesModule,
     LlmModule,
   ],
   controllers: [AppController],
