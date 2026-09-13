4786e0dc6c7099620accf5cd7a70ba27bb8ca6d6
Heindrich-Jansen <heindrichjansen@gmail.com>
2026-09-13 17:23:17 +0200

    feat: add passed flag to EducationPayload and conditionally handle event recording

--- project/backend/analytics-service/src/analytics/analytics.controller.ts
+++ project/backend/analytics-service/src/analytics/analytics.controller.ts
@@ -64,6 +64,7 @@ interface EducationPayload {
 
   assignmentId?: string;
   reportId?: string;
+  passed?: boolean;
 }
 
 interface MailingPayload {
@@ -167,6 +168,10 @@ export class AnalyticsController {
   })
   async onEducationAssigned(payload: EducationPayload) {
     //console.log('edu completed', payload); //debugging
+    if (payload.passed !== true) {
+      return;
+    }
+
     await this.analyticsService.recordEvent({
       eventType: AnalyticsEventType.EDUCATION_ASSIGNED,
       auth0Id: payload.auth0Id,
