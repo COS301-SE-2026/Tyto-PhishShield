069e65b67632696e6ad221c4bbf6aa3c0a36eb24
Josua-Louw <u24569772@tuks.co.za>
2026-09-14 20:03:42 +0200

    added integration testing

--- project/backend/comms-tracking/test/app.e2e-spec.ts
+++ project/backend/comms-tracking/test/app.e2e-spec.ts
@@ -3,12 +3,29 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+
+jest.setTimeout(120000);
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let postgres: StartedPostgreSqlContainer;
+
+  beforeAll(async () => {
+    postgres = await new PostgreSqlContainer('postgres:15-alpine')
+      .withDatabase('comms_test')
+      .withPassword('test_user')
+      .withPassword('test_password').start()
+
+    process.env.DB_HOST = postgres.getHost();
+    process.env.DB_PORT = String(postgres.getPort());
+    process.env.DB_USERNAME = postgres.getUsername();
+    process.env.DB_PASSWORD = postgres.getPassword();
+    process.env.DB_NAME = postgres.getDatabase();
+  });
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+      const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
@@ -25,5 +42,9 @@ describe('AppController (e2e)', () => {
 
   afterEach(async () => {
     await app.close();
+  });
+
+  afterAll(async () => {
+    await postgres?.stop();
   });
 });
